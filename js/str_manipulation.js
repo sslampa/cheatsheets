457278e2d5d14440bab3edb47133064a61bb50e2
Stephen Lampa <sslampa@gmail.com>
2016-12-10 15:59:27 -0800

    Added str_manipulation.js

---
+++ js/str_manipulation.js
@@ -0,0 +1,22 @@
+//.toLowerCase
+//.toUpperCase
+//.trim
+//.trimLeft
+//.trimRight
+
+var someStr = "   stringTObeMANIPULATED     ";
+
+// Changes string to all lowercase
+console.log(someStr.toLowerCase()); //"   stringtobemanipulated     "
+
+// Changes string to all uppercase
+console.log(someStr.toUpperCase()); //"   STRINGTOBEMANIPULATED     "
+
+// Removes whitespace on both sides
+console.log(someStr.trim()); //"stringTObeMANIPULATED"
+
+// Removes whitespace to the left of string
+console.log(someStr.trimLeft()); //"stringTObeMANIPULATED     "
+
+// Removes whitespace to the right of string
+console.log(someStr.trimRight()); //"   stringTObeMANIPULATED"
